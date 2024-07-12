4f67acbe063b864ad7d06c068492d4a6f3ce2295
Melike Sinem Kaya <msinemkaya6@gmail.com>
2024-07-12 20:24:27 +0300

    feat: changed yup schema validation messages

--- src/utils/yupAuthSchema.js
+++ src/utils/yupAuthSchema.js
@@ -1,17 +1,17 @@
 import { object, string } from 'yup'
 
 const schema = object({
-  email: string().required('No email provided.').email('Invalid email format'),
+  email: string().required('You should provide an email.').email('Invalid email format.'),
   password: string()
-    .min(8, 'Password is too short should be at least 8 characters.')
+    .min(8, 'Password is too short should be minimum 8 characters.')
     .matches(/[a-z]/, 'Password must contain at least one lowercase letter.')
     .matches(/[A-Z]/, 'Password must contain at least one uppercase letter.')
     .matches(/\d/, 'Password must contain at least one number.')
     .matches(/[!@#$%^&*(),.?":{}|<>]/, 'Password must contain at least one special character.')
-    .required('No password provided.'),
+    .required('You should provide a password.'),
   userType: string()
-    .oneOf(['teacher', 'student'], 'Please select a valid user type')
-    .required('User type is required'),
+    .required('User type is required')
+    .oneOf([ 'teacher', 'student' ], 'Please select a valid user type'),
 })
 
 export default schema
