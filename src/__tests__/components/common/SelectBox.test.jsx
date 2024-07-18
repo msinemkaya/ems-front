62e1c0364248f25c389ddeece98154a0d74c55ff
Melike Sinem Kaya <msinemkaya6@gmail.com>
2024-07-18 23:21:06 +0300

    feat: SelectBox test and SelectBox rearrangement

---
+++ src/__tests__/components/common/SelectBox.test.jsx
@@ -0,0 +1,40 @@
+import '@testing-library/jest-dom'
+import { fireEvent, render, screen } from '@testing-library/react'
+import { SelectBox } from '../../../components/common/SelectBox.jsx'
+
+describe('SelectBox', () => {
+  it('renders select box with correct label', () => {
+    render(<SelectBox label="select" options={[]}/>)
+
+    const selectElement = screen.getByText('select')
+    expect(selectElement).toBeInTheDocument()
+  })
+
+  it('renders select box with correct options', () => {
+    const options = [ 'foo', 'bar' ]
+
+    render(<SelectBox label="select" options={options}/>)
+
+    options.forEach(option => {
+      const optionElement = screen.getByText(option)
+      expect(optionElement).toBeInTheDocument()
+    })
+  })
+
+  it('renders select box with error message', () => {
+    render(<SelectBox label="select" options={[]} errors='error message'/>)
+
+    const selectElement = screen.getByText('error message')
+    expect(selectElement).toBeInTheDocument()
+  })
+
+  it('handles option change correctly', () => {
+    const options = [ 'foo', 'bar' ]
+
+    render(<SelectBox label="select" options={options}/>)
+    const selectElement = screen.getByRole('combobox');
+    fireEvent.change(selectElement, { target: { value: 'foo' } });
+
+    expect(selectElement.value).toBe('foo');
+  })
+})
--- src/components/common/SelectBox.jsx
+++ src/components/common/SelectBox.jsx
@@ -2,10 +2,11 @@ export const SelectBox = ({ options, label, errors }) => {
   return (
     <div>
       <select
+        defaultValue='label'
         className={`border-r-8 border-transparent outline outline-1 px-2.5 py-2.5 rounded-lg w-full capitalize 
         ${errors ? 'text-red-500 outline-red-500' : 'text-secondary outline-secondary'} text-sm`}
         id="userType" name="userType">
-        <option selected hidden>{label}</option>
+        <option value='label' disabled hidden>{label}</option>
         {options.map(option => (
           <option key={option} value={option}>{option}</option>
         ))}
